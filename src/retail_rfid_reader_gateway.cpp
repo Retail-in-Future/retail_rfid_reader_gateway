54d6cf15222ca8423a081e00dc17d02a4c3b6530
Cheng Tang <ctang@thoughtworks.com>
2017-06-20 22:33:28 +0800

    add support to aws iot sdk cpp, and use logging to validate it.

--- src/retail_rfid_reader_gateway.cpp
+++ src/retail_rfid_reader_gateway.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <util/logging/Logging.hpp>
+#include <util/logging/LogMacros.hpp>
+#include <util/logging/ConsoleLogSystem.hpp>
+#include <ClientCore.hpp>
 
 int main(int argc, char *argv[])
 {
+    std::shared_ptr<awsiotsdk::util::Logging::ConsoleLogSystem> p_log_system =
+        std::make_shared<awsiotsdk::util::Logging::ConsoleLogSystem>(awsiotsdk::util::Logging::LogLevel::Info);
+    awsiotsdk::util::Logging::InitializeAWSLogging(p_log_system);
+    awsiotsdk::util::Logging::ShutdownAWSLogging();
     return 0;
 }
