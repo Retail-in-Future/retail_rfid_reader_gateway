dc542c14baf19b374d315fa315a940eebfaffd79
Cheng Tang <ctang@thoughtworks.com>
2017-06-23 17:34:12 +0800

    add support for boost libraries

--- src/retail_rfid_reader_gateway.cpp
+++ src/retail_rfid_reader_gateway.cpp
@@ -2,10 +2,13 @@
 #include <util/logging/Logging.hpp>
 #include <util/logging/LogMacros.hpp>
 #include <util/logging/ConsoleLogSystem.hpp>
-#include <ClientCore.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/foreach.hpp>
 
 using namespace std;
 using namespace awsiotsdk::util::Logging;
+using namespace boost::property_tree;
 
 int main(int argc, char *argv[])
 {
@@ -13,5 +16,10 @@ int main(int argc, char *argv[])
         make_shared<ConsoleLogSystem>(LogLevel::Info);
     InitializeAWSLogging(p_log_system);
     ShutdownAWSLogging();
+    ptree pt;
+    BOOST_FOREACH(const ptree::value_type &v, pt.get_child("sked")) {
+        if (v.first == "flight") {
+        }
+    }
     return 0;
 }
