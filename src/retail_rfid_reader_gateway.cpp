cd954d4811cec03f49f228eefc5fdef32fe733d8
Cheng Tang <ctang@thoughtworks.com>
2017-06-28 22:50:51 +0800

    modify main function to use Options to parse command line options.

--- src/retail_rfid_reader_gateway.cpp
+++ src/retail_rfid_reader_gateway.cpp
@@ -2,27 +2,21 @@
 #include <util/logging/Logging.hpp>
 #include <util/logging/LogMacros.hpp>
 #include <util/logging/ConsoleLogSystem.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-#include <boost/foreach.hpp>
-#include <boost/core/ignore_unused.hpp>
+#include <boost/smart_ptr.hpp>
+#include "parameters/options.hpp"
 
 using namespace std;
-using namespace awsiotsdk::util::Logging;
 using namespace boost;
-using namespace boost::property_tree;
+using namespace awsiotsdk::util::Logging;
 
 int main(int argc, char *argv[])
 {
-    ignore_unused(argc, argv);
+    {
+        auto options = boost::make_shared<Options>(argc, (char const **)argv);
+    }
     std::shared_ptr<ConsoleLogSystem> p_log_system =
         std::make_shared<ConsoleLogSystem>(LogLevel::Info);
     InitializeAWSLogging(p_log_system);
     ShutdownAWSLogging();
-    ptree pt;
-    BOOST_FOREACH(const ptree::value_type &v, pt.get_child("sked")) {
-        if (v.first == "flight") {
-        }
-    }
     return 0;
 }
