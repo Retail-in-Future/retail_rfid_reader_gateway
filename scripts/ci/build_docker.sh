14d27506d8d4c9f14e68b887182d5ad4d200c14b
Cheng Tang <ctang@thoughtworks.com>
2017-07-21 14:01:07 +0800

    modify docker file to reduce docker image build time.

--- scripts/ci/build_docker.sh
+++ scripts/ci/build_docker.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 #
-# SCRIPT: build_docker
-# AUTHOR: Tang Cheng
-# DATE:   2017/07/17
-# REV:    1.0.D (Valid are A, B, D, T, Q, and P)
-#               (For Alpha, Beta, Dev, Test, QA, and Production)
-#
-# PLATFORM: Linux
-#
-# PURPOSE: Build a docker environment for test.
-# 
-#
 # set -n   # Uncomment to check script syntax, without execution.
 #          # NOTE: Do not forget to put the # comment back in or
 #          #       the shell script will never execute!
@@ -36,7 +25,7 @@ pushd $top_dir
 
 app_name=$(basename $(pwd))
 docker_image_id=`echo "local/${app_name}:latest" | tr "[:upper:]" "[:lower:]"`
-docker build -t $docker_image_id -f dockers/fedora25-x86_64-Dockerfile .
+docker build -t $docker_image_id -f dockers/Dockerfile .
 
 popd
 
